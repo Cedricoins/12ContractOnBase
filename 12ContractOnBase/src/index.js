22c155a74a4c29b790b0de3fb213c5ddeee634cc
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-10-07 15:57:08 +0200

    Update index.js

--- 12ContractOnBase/src/index.js
+++ 12ContractOnBase/src/index.js
@@ -2,7 +2,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css"; // pour Tailwind ou ton style global
 
 // Récupère la div <div id="root"></div> dans index.html
 const container = document.getElementById("root");
