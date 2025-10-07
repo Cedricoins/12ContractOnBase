3b0bea7af0dd039b48893001d0d79e4fa4c7babe
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-10-07 18:59:55 +0200

    Update Salut.sol

--- Salut.sol
+++ Salut.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-/// Contrat 5 : dire "salut"
+
 contract Salut {
-    function dire() public pure returns (string memory) {
+    event MessageEnvoye(address indexed expediteur, string message);
+
+    function dire() public returns (string memory) {
+        emit MessageEnvoye(msg.sender, "salut");
         return "salut";
     }
 }
