54d602e02af61c0b83f67b74a882c99f41771c5e
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-10-07 19:01:32 +0200

    Update non.sol

--- non.sol
+++ non.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-/// Contrat 10 : dire "non"
+
 contract NonContract {
-    function dire() public pure returns (string memory) {
+    event MessageEnvoye(address indexed expediteur, string message);
+
+    function dire() public returns (string memory) {
+        emit MessageEnvoye(msg.sender, "non");
         return "non";
     }
 }
