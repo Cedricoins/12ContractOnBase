eb25c74ed8422f47ddb4a98fcfd861f89f0aa43a
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-07 18:11:10 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// index.js
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+// Récupère la div <div id="root"></div> dans index.html
+const container = document.getElementById("root");
+
+// Crée le “root” React moderne
+const root = createRoot(container);
+
+// Monte ton application React
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
