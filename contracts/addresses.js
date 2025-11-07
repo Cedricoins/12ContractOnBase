ef51ed894e20d6276fec36fa6997b87934cb83cf
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-07 18:13:44 +0100

    Create addresses.js

---
+++ contracts/addresses.js
@@ -0,0 +1,14 @@
+export const CONTRACT_ADDRESSES = {
+  Incrementeur: "0x00a378cf69c9e8686aad6be7d4f3115c6f2a5768",
+  Decrementeur: "0x67f5fe6c5abcf3f12f51dfbb2189c1117858ff3b",
+  Bonjour: "0x3d2669cc6a3139e41314022c5a499a2555c831d2",
+  BonneNuit: "0xd78d4a437675e78d2c3844d1b8a9e80e274844f1",
+  Salut: "0xc5f47f49e4f74c9caf970267d10dfc4905d81e35",
+  TUContract: "0xa0c5190483735c3e3246df5bdecb29867a4a8939",
+  VaContract: "0x773b2ef6dd50eb0a260765f6229067aab7698d81",
+  BienContract: "0x613a53e64533da9362a9e33c7146d6b843e0ad2c",
+  OuiContract: "0x92a3a10b1a26117d03dc92ef316f8ba347877055",
+  NonContract: "0x5afe45a0cc74156c03d4576b6ae6c7deeedcaafd",
+  MerciContract: "0x1e88cb97eba69114809da395417e3b9195b190a5",
+  PasMerciContract: "0xee3617406aa3984074410deac0c895f20fbf8211"
+};
